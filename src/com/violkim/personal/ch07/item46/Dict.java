21d618c00f74b72b178c171c5bc7dc93ae686b58
nicechester <violkim@gmail.com>
2018-02-13 20:42:21 -0800

    uncomment mistakenly commented blocks

--- src/com/violkim/personal/ch07/item46/Dict.java
+++ src/com/violkim/personal/ch07/item46/Dict.java
@@ -12,23 +12,23 @@ import static java.util.stream.Collectors.groupingBy;
 
 public class Dict {
     // Uses the streams API but not the paradigm--Don't do this!
-//    private Map<String, Long> wordFrequencySideEffect(File file) throws FileNotFoundException {
-//        final Map<String, Long> freq = new HashMap<>();
-//        try (Stream<String> words = new Scanner(file).tokens()) {
-//            words.forEach(word -> {
-//                freq.merge(word.toLowerCase(), 1L, Long::sum);
-//            });
-//        }
-//        return freq;
-//    }
-//
-//    private Map<String, Long> wordFrequencyPureFunctional(File file) throws FileNotFoundException {
-//        // Proper use of streams to initialize a frequency table
-//        Map<String, Long> freq;
-//        try (Stream<String> words = new Scanner(file).tokens()) {
-//            freq = words.collect(groupingBy(String::toLowerCase, counting()));
-//        }
-//        return freq;
-//    }
+    private Map<String, Long> wordFrequencySideEffect(File file) throws FileNotFoundException {
+        final Map<String, Long> freq = new HashMap<>();
+        try (Stream<String> words = new Scanner(file).tokens()) {
+            words.forEach(word -> {
+                freq.merge(word.toLowerCase(), 1L, Long::sum);
+            });
+        }
+        return freq;
+    }
+
+    private Map<String, Long> wordFrequencyPureFunctional(File file) throws FileNotFoundException {
+        // Proper use of streams to initialize a frequency table
+        Map<String, Long> freq;
+        try (Stream<String> words = new Scanner(file).tokens()) {
+            freq = words.collect(groupingBy(String::toLowerCase, counting()));
+        }
+        return freq;
+    }
 
 }
